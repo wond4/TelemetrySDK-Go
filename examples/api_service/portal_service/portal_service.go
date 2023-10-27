89c2ee3c88962182749fcd4992bfbb4673c81a01
谢梅生 <xie.meisheng@aishu.cn>
2023-10-27 18:20:18 +0800

    [feature] 典型api微服务代码示例

--- examples/api_service/portal_service/portal_service.go
+++ examples/api_service/portal_service/portal_service.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/address", func(c *gin.Context) {
-		c.String(http.StatusOK, "OK")
+	r.GET("/users/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		name := getUser(id)
+		maskedName := desensitizeUserName(name)
+		c.String(http.StatusOK, maskedName)
 	})
-	err := r.Run("127.0.0.1:50080")
-	if err != nil {
-		println(err)
+	_ = r.Run(":50080")
+}
+
+// desensitizeUserName 用户名称脱敏
+func desensitizeUserName(name string) string {
+	runes := []rune(name)
+
+	for i := 1; i < len(runes); i++ {
+		runes[i] = '*'
 	}
+
+	return string(runes)
+}
+
+// getUser 调用其他服务，根据用户ID获取用户名称
+func getUser(id string) string {
+	client := http.Client{}
+
+	url := fmt.Sprintf("http://127.0.0.1:50081/users/%s", id)
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println("请求失败:", err)
+		return ""
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("读取响应失败:", err)
+		return ""
+	}
+
+	return string(body)
 }
